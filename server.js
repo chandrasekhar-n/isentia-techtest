978a74ea42c3ee6ea56a1c262abd475f0a0b8634
chandrasekhar-n <cnandigama@woolworths.com.au>
2019-06-15 20:54:27 +1000

    Testing the Flicker API call and conversion to JSON

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const https = require('https')
+const parseString = require('xml2js').parseString
 const app = express();
 
-app.get('/').get((req, res) => {
-    res.send("Hello World");
+app.route('/api/flickerFeed').get((req, res) => {
+    https.get('https://api.flickr.com/services/feeds/photos_public.gne',(resp) =>{
+        let data='';
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        resp.on('error',function (e) {
+            console.error(e);
+        });
+
+        resp.on('end',() =>{
+            parseString(data, function (err, result) {
+                console.error(err);
+                res.send(result);
+            });
+        });
+    });
 });
 
 const port = process.env.PORT || 3000;
